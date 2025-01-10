31b262da3498f5102f153be36d1e43eacff03a18
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2025-01-10 02:33:28 -0800

    watchduty data dwonload

--- download_evac.ts
+++ download_evac.ts
@@ -33,3 +33,53 @@ fetch(url)
     })
     .catch(error => console.error(error));
 
+    /*Format is like
+
+    [
+        {
+            "id": 31,
+            "name": "orange-CA_US",
+            "slug": "orange-CA_US",
+            "is_reporter_covered_for_fire": true,
+            "state": "CA",
+            "display_name": "Orange County",
+            "state_display_name": "California",
+            "date_created": "2022-05-31T17:37:42Z",
+            "date_modified": "2023-02-01T22:57:37Z",
+            "is_active": true
+          },
+    ]
+    */
+
+fetch("https://api.watchduty.org/api/v1/regions")
+.then(response => response.json())
+.then(data => {
+    //filter for state == CA
+
+    const california_regions = data.filter(region => region.state == "CA");
+
+    let region_ids = california_regions.map(region => region.id);
+
+    fetch("https://api.watchduty.org/api/v1/evac_zones?region_ids=" + region_ids.join(","))
+    .then(response => response.json())
+    .then(data => {
+        //save to data/evac_zones_watchduty.json
+
+        const data_dir = path.join(__dirname, 'data');
+
+        if (!fs.existsSync(data_dir)) {
+            fs.mkdirSync(data_dir);
+        }
+
+        const file_path = path.join(data_dir, 'evac_zones_watchduty.json');
+
+        fs.writeFile(file_path, JSON.stringify(data), (error) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            console.log("Watchduty data saved to file");
+        });
+    })
+    .catch(error => console.error(error));
+})
